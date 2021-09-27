30f3cb439e5987c10b50a5d426a04542376ab96c
hiyongz <zhiyo2016@163.com>
2021-09-27 15:17:27 +0800

    更新配置文件

---
+++ product30/2231275.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=testcase-001
+cid=2231275
+pid=30
+
+step1 >> 1
+step2 >> 2
+
+*/
+
+// 访问'demo\php'获取更多帮助
+
+?>
---
+++ product30/2231276.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+/**
+
+title=testcase-002
+cid=2231276
+pid=30
+
+step1 >> 1
+step2 >> 2
+
+*/
+
+// 访问'demo\php'获取更多帮助
+
+?>
